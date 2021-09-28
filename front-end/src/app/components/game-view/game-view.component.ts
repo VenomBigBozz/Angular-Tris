f9e5a6a30d0a4aea7331311a193e5ffea892a393
venombigbozz <riccardo.n1402@gmail.com>
2021-09-28 17:53:16 +0200

    revert last commit (angular is wholesome)

--- front-end/src/app/components/game-view/game-view.component.ts
+++ front-end/src/app/components/game-view/game-view.component.ts
@@ -8,14 +8,12 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class GameViewComponent implements OnInit {
   @ViewChild(BoardComponent) board: BoardComponent;
-  vsAiPlayer: boolean = null;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   newGame(aiPlayer: boolean): void {
-    this.vsAiPlayer = aiPlayer;
     this.board.newGame(aiPlayer);
   }
 }
