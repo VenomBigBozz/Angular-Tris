fa2234360c3c5ae85c91f4b577382d402b97686c
Riccardo Nuncibello <riccardo.n1402@gmail.com>
2021-11-24 20:10:29 +0000

    eliminated backend

--- back-end/API/src/main/kotlin/com/tris/api/repository/MatchRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.tris.api.repository
-
-import com.tris.api.model.Match
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MatchRepository : JpaRepository<Match, Long>
